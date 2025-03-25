24da0d1c26901cd38be13c52747a6422114a5c42
Kevin Correa <gdlkcorr@gmail.com>
2025-03-25 12:55:07 -0600

    Fixing code smells

--- Java/src/main/java/config/Runner.java
+++ Java/src/main/java/config/Runner.java
@@ -1,3 +1,5 @@
+package config;
+
 import models.MilitaryType;
 import Planes.MilitaryPlane;
 import Planes.PassengerPlane;
@@ -28,13 +30,13 @@ public class Runner {
         Airport airport = new Airport(planes);
         Airport militaryAirport = new Airport(airport.getMilitaryPlanes());
         Airport passengerAirport = new Airport(airport.getPasPl());
-        System.out.println("Military airport sorted by max distance: " + militaryAirport
+        logger.log("Military airport sorted by max distance: " + militaryAirport
                 .sortByMaxDistance()
                 .toString());
-        System.out.println("Passenger airport sorted by max speed: " + passengerAirport
+        logger.log("Passenger airport sorted by max speed: " + passengerAirport
                 .sortByMaxSpeed()
                 .toString());
 
-        System.out.println("Plane with max passenger capacity: " + passengerAirport.getPassengerPlaneWithMaxPassengersCapacity());
+        logger.log("Plane with max passenger capacity: " + passengerAirport.getPassengerPlaneWithMaxPassengersCapacity());
     }
 }
